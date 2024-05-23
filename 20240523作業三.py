195db9102bfdbc49a933422cef14f200a68c9c92
ellenpiu <ellenchuang18@gmail.com>
2024-05-23 11:12:59 +0800

    Create 20240523作業三.py

---
+++ 20240523作業三.py
@@ -0,0 +1,16 @@
+def is_odd(x):
+    if x % 2 == 1:
+        return True
+    else:
+        return False
+
+print(is_odd(33))
+print(is_odd(546))
+
+
+True
+False
+
+
+def is_odd(x):
+    return x % 2 ==1
